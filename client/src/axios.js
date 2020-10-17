4b7e156a513a19171d7ab7050a53933a6a19eb52
jake <9012257+jeiku@users.noreply.github.com>
2020-10-17 16:06:44 -0400

    created orders and connected to back end

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/clone-9ecc3/us-central1/api", // THE API (cloud function) URL
+  // THE API (cloud function) URL
+  baseURL: "https://us-central1-clone-9ecc3.cloudfunctions.net/api",
+  // http://localhost:5001/clone-9ecc3/us-central1/api
 });
 
 export default instance;
--- client/src/components/Orders.js
+++ client/src/components/Orders.js
@@ -1,16 +1,42 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {db} from "../firebase";
+import {useStateValue} from "../StateProvider";
 import "./Orders.css";
+import Order from "./Order";
 
 function Orders() {
-  const [orders, setOrders] = useStatete([]);
+  const [{basket, user}, dispatch] = useStateValue();
+  const [orders, setOrders] = useState([]);
 
   useEffect(() => {
-    db.collection("users");
-  }, []);
+    if (user) {
+      // go into db, users, get user and their orders, order it in descending order, take a realtime snapshot of it
+      // for each document, return an object with id, data
+      db.collection("users")
+        .doc(user?.uid)
+        .collection("orders")
+        .orderBy("created", "desc")
+        .onSnapshot((snapshot) =>
+          setOrders(
+            snapshot.docs.map((doc) => ({
+              id: doc.id,
+              data: doc.data(),
+            }))
+          )
+        );
+    } else {
+      setOrders([]);
+    }
+  }, [user]);
+
   return (
     <div className='orders'>
       <h1>Your Orders</h1>
+      <div className='orders__order'>
+        {orders.map((order) => (
+          <Order order={order} />
+        ))}
+      </div>
     </div>
   );
 }
